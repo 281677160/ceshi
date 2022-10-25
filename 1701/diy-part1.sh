93dd54c44dd058b967823eb95bc57ff9da7e4fe0
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-25 12:34:27 +0800

    Update diy-part1.sh

--- 1701/diy-part1.sh
+++ 1701/diy-part1.sh
@@ -13,6 +13,7 @@
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2  package/redsocks2
 svn co https://github.com/coolsnowwolf/packages/trunk/net/baidupcs-web package/baidupcs-web
+svn co https://github.com/coolsnowwolf/packages/trunk/devel/go-rice package/go-rice
 git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
