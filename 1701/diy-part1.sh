5060eafeb0ddaee66aacd93696da2a06f0ac19a3
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-25 12:12:04 +0800

    Update diy-part1.sh

--- 1701/diy-part1.sh
+++ 1701/diy-part1.sh
@@ -9,3 +9,10 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2  package/redsocks2
+svn co https://github.com/coolsnowwolf/packages/trunk/net/baidupcs-web package/baidupcs-web
+git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
