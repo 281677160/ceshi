7c8d8ed981c7c766c1414501cdc12494a8e10b52
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-26 00:34:20 +0800

    Update diy-part1.sh

--- 1701/diy-part1.sh
+++ 1701/diy-part1.sh
@@ -10,15 +10,15 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-svn export https://github.com/Lienol/openwrt/branches/21.02/package/default-settings package/default-settings
+svn export https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/default-settings package/default-settings
 
 svn export https://github.com/281677160/luci-theme-argon/branches/21.02 package/luci-theme-argon
-svn export https://github.com/281677160/luci-theme-argon/branches/argon-config package/luci-app-argon-config
+svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 svn export https://github.com/immortalwrt/packages/trunk/net/redsocks2 package/redsocks2
 git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-openclash package/luci-app-openclash
 
 
 curl -fsSL https://raw.githubusercontent.com/281677160/common/main/Convert/zh_Hans.sh > zh_Hans.sh
