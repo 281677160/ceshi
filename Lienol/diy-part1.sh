9c0d255627473b20ccc3249fe54f96f144f27aa8
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-25 21:43:19 +0800

    Update diy-part1.sh

--- Lienol/diy-part1.sh
+++ Lienol/diy-part1.sh
@@ -12,6 +12,8 @@
 
 svn export https://github.com/281677160/luci-theme-argon/branches/18.06 package/luci-theme-argon
 svn export https://github.com/281677160/luci-theme-argon/branches/argon-config package/luci-app-argon-config
+
+git clone -b master --depth 1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
