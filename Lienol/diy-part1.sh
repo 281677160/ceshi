bd05165f079e462e2bc5fcb1f6f0cc2dfca0dbe1
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-26 12:22:49 +0800

    Update diy-part1.sh

--- Lienol/diy-part1.sh
+++ Lienol/diy-part1.sh
@@ -9,12 +9,11 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-rm -rf package/lean
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
+
 svn export https://github.com/281677160/luci-theme-argon/branches/18.06 package/luci-theme-argon
 svn export https://github.com/281677160/luci-theme-argon/branches/argon-config package/luci-app-argon-config
 
-git clone -b master --depth 1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+
 git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
