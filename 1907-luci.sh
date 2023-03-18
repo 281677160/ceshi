4100c81f1618dc04660c59ea23e9051a85154964
281677160 <66651997+281677160@users.noreply.github.com>
2023-03-18 20:09:15 +0800

    Update 1907-luci.sh

--- 1907-luci.sh
+++ 1907-luci.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
