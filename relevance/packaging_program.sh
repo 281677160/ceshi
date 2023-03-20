86b4b58ff1377b8af568f1e0b80888bd94eb0e3d
281677160 <66651997+281677160@users.noreply.github.com>
2023-03-20 11:44:39 +0800

    Update packaging_program.sh

--- relevance/packaging_program.sh
+++ relevance/packaging_program.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+function Package_defaultsettings() {
+ZZZL_PATH="$(find "$GITHUB_WORKSPACE/openwrt/package" -type d -name "default-settings")"
+if [[ -d "${ZZZL_PATH}" ]]; then
+  echo "ZZZ_PATH=$(find "$GITHUB_WORKSPACE/openwrt/package" -type f -name "*-default-settings")" >> $GITHUB_ENV
+else
+  cp -rf $GITHUB_WORKSPACE/settings $GITHUB_WORKSPACE/openwrt/package/default-settings
+  echo "ZZZ_PATH=$(find "$GITHUB_WORKSPACE/openwrt/package" -type f -name "*-default-settings")" >> $GITHUB_ENV
+fi
+GENERATE_PATH="${GITHUB_WORKSPACE}/openwrt/package/base-files/files/bin/config_generate"
+echo "GENERATE_PATH=${GENERATE_PATH}" >> $GITHUB_ENV
+echo "IPADDR=$(grep "ipaddr:-" "${GENERATE_PATH}" |grep -v 'addr_offset' |grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")" >> $GITHUB_ENV
+}
+
+
+function Diy_pattsh() {
+./scripts/feeds update -a > /dev/null 2>&1
+[[ -f "$GITHUB_WORKSPACE/feeds.conf.default" ]] && mv -f $GITHUB_WORKSPACE/feeds.conf.default feeds.conf.default
+[[ -d "$GITHUB_WORKSPACE/diy" ]] && cp -Rf $GITHUB_WORKSPACE/diy/* $GITHUB_WORKSPACE/openwrt/
+[[ -d "$GITHUB_WORKSPACE/files" ]] && mv -f $GITHUB_WORKSPACE/files/* files
+if [[ -d "$GITHUB_WORKSPACE/patches" ]]; then
+  find "$GITHUB_WORKSPACE/patches" -type f -name '*.patch' -print0 | sort -z | xargs -I % -t -0 -n 1 sh -c "cat '%'  | patch -d './' -p1 --forward --no-backup-if-mismatch"
+fi
+sudo chmod +x $GITHUB_WORKSPACE/${DIY_PART_SH}
+source $GITHUB_WORKSPACE/${DIY_PART_SH}
+if [[ "${env.DIY_PART_SH}" == "1907-luci.sh" ]]; then
+  cp -Rf $GITHUB_WORKSPACE/relevance/zh_Hans.sh ${HOME_PATH}/zh_Hans.sh
+  sudo chmod +x ${HOME_PATH}/zh_Hans.sh
+  /bin/bash ${HOME_PATH}/zh_Hans.sh
+fi
+echo "amlogic_model=${amlogic_model}" >> ${GITHUB_ENV}
+echo "amlogic_kernel=${amlogic_kernel}" >> ${GITHUB_ENV}
+echo "auto_kernel=${auto_kernel}" >> ${GITHUB_ENV}
+echo "rootfs_size=${rootfs_size}" >> ${GITHUB_ENV}
+echo "kernel_repo=${kernel_repo}" >> ${GITHUB_ENV}
+}
+
 function Packaged_services() {
 FOLDER_NAME="${GITHUB_WORKSPACE}/REPOSITORY"
 TRIGGER_PROGRAM="${FOLDER_NAME}/relevance"
