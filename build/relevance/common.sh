84d843542cb2721c1a1933d2060a9cba07f53185
281677160 <66651997+281677160@users.noreply.github.com>
2023-03-22 01:03:21 +0800

    Update common.sh

--- build/relevance/common.sh
+++ build/relevance/common.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 function Package_python3() {
-sudo apt-get update
-sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.5-dev
-sudo apt-get --reinstall install python3.5-minimal
-sudo mv /usr/bin/python3 /usr/bin/python3-old
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 300
+sudo apt-get install python3.5
+whereis python3.5
 python3 -V
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 300
 }
 
 function Package_settings() {
