bda131d02b6feccb5da19d5c006bd05ebf3888a0
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-07 12:44:00 +0800

    Update common.sh

--- build/devices/relevance/common.sh
+++ build/devices/relevance/common.sh
@@ -231,7 +231,7 @@ echo -e "\033[32m 应用文件: ${FOLDER_NAME} \033[0m"
 echo -e "\033[32m 配置文件: $(echo "${CONFIG_FILE}" |cut -d"/" -f2) \033[0m"
 echo -e "\033[32m 扩展文件: ${DIY_PART_SH} \033[0m"
 echo -e "\033[32m 编译机型: ${TARGET_PROFILE} \033[0m"
-echo -e "\033[32m 编译内核: ${LINUX_KERNEL} \033[0m"
+echo -e "\033[32m 内核版本: ${LINUX_KERNEL} \033[0m"
 echo
 echo -e "\033[35m 服务器CPU: ${CPU_MODEL} \033[0m"
 echo
