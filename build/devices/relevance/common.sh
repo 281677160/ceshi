c46993ec27802ff5fb2c3fb0a971b0ea10f45c8b
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-07 13:03:41 +0800

    Update common.sh

--- build/devices/relevance/common.sh
+++ build/devices/relevance/common.sh
@@ -258,5 +258,13 @@ fi
 echo -e "\033[33m 清理[X]天之前的Artifacts: ${RETAIN_DAYS}天 \033[0m"
 echo -e "\033[33m 保留[X]个Releases不被删除: ${KEEP_LATEST}个 \033[0m"
 echo
+if [[ "${PACKAGING_FIRMWARE}" == "true" ]] && [[ "${TARGET_PROFILE}" == "Armvirt_64" ]]; then
+echo -e "\033[34m amlogic_model: ${amlogic_model} \033[0m"
+echo -e "\033[34m amlogic_kernel: ${amlogic_kernel} \033[0m"
+echo -e "\033[34m auto_kernel: ${auto_kernel} \033[0m"
+echo -e "\033[34m rootfs_size: ${rootfs_size} \033[0m"
+echo -e "\033[34m kernel_repo: ${kernel_repo} \033[0m"
+fi
+echo
 echo
 }
